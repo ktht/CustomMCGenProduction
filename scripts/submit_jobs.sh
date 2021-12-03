8e4efb5d924be0850ca5aa841f9482a9a3a9d946
ktht <karlehataht@gmail.com>
2021-12-04 01:49:07 +0200

    Parameters for more convenient job submission

--- scripts/submit_jobs.sh
+++ scripts/submit_jobs.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# submits 4k jobs, each processing 2k events
+# submit_jobs.sh 1 1000 2000
+# submits 4x1000=4000 jobs, each processing 2k events
+
+IDX1=$1
+IDX2=$2
+NOF_EVENTS=$3
+
+if [ "$IDX1" -gt "$IDX1" ]; then
+  echo "Invalid parameters: $IDX1, $IDX2"
+  exit 1;
+fi
 
 for cfg in ttbarDL_orig ttbarDL_decayAll hhDL_orig hhDL_decayAll; do
   logdir=/home/karl/CustomProduction/$cfg/logs;
   mkdir -p $logdir;
-  for i in `seq 1 1000`; do
+  for i in `seq $IDX1 $IDX2`; do
     sbatch --partition=main --output=$logdir/out_$i.log \
       $HOME/CMSSW_7_1_26/src/Configuration/CustomCards/scripts/job_wrapper.sh \
-      $i fragment_$cfg.py 2000 /hdfs/local/karl/CustomProduction/$cfg;
+      $i fragment_$cfg.py $NOF_EVENTS /hdfs/local/karl/CustomProduction/$cfg;
   done
 done
